e4a14016f58b1a15603fc4f6c88a982a0044addc
E987270_ibe <jbriones.ext+IB@ayesa.com>
2026-06-18 17:48:44 +0200

    modificamos el modelo para pruebas de despliegue

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,6 @@ class User < ApplicationRecord
   enum :role, { basic: 0, subscriber: 1, admin: 2 }, default: :basic
 
   validates :fullName, presence: true
-
   validates :dni, presence: true, uniqueness: true
   validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
   validates :date_of_birth, presence: true
